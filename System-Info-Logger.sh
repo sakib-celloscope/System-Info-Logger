322edfc4532f5da11e8a02f525f74507fbcc33b9
sakib-celloscope <sakib.celloscope@gmail.com>
2023-09-10 16:38:44 +0600

    initialized git and created a shell script file

---
+++ System-Info-Logger.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#System Info Logger
+#System Info Logger is a shell script that automates the process of gathering essential system information, including RAM usage, available storage, and OS details. It logs this data into a file and sends it via email, providing daily insights into your system's health and status.
+
+echo "System Info Logger"
