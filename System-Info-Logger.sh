c89eeef14f7f04c94e9df0e2d91e530c19f6d002
sakib-celloscope <sakib.celloscope@gmail.com>
2023-09-10 18:18:12 +0600

    created automated log file to store those info

--- System-Info-Logger.sh
+++ System-Info-Logger.sh
@@ -3,8 +3,20 @@
 #System Info Logger
 #System Info Logger is a shell script that automates the process of gathering essential system information, including RAM usage, available storage, and OS details. It logs this data into a file and sends it via email, providing daily insights into your system's health and status.
 
+#Check Current Directory
+CURRENT_DIRECTORY=`pwd`
+
+#Checking Date in YYYY-MM-DD Format
+CURRENT_DATE=`date +%F`
+
+#Checking Current Time
+CURRENT_TIME=`date +%T`
+
+echo -e "Script Executed at ${CURRENT_TIME}\n"
+
+
 #Printing the Script Name
-echo -e "Script Name: ${0}\n"
+echo -e "${0} Executed at ${CURRENT_TIME}\n" >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
 
 #RAM: Displays information of RAM
 RAM(){
@@ -13,9 +25,9 @@ RAM(){
     USED_RAM_SPACE=`free -mt | grep "Total" | awk '{print $3}'`
 
 
-    echo "Total Ram Size: ${TOTAL_RAM} MB"
-    echo "Total Free Space: ${FREE_RAM_SPACE} MB"
-    echo -e "Total Used Space: ${USED_RAM_SPACE} MB\n"
+    echo "Total Ram Size: ${TOTAL_RAM} MB" >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
+    echo "Total Free Space: ${FREE_RAM_SPACE} MB" >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
+    echo -e "Total Used Space: ${USED_RAM_SPACE} MB\n" >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
 }
 
 #STORAGE: Displays information of Storage
@@ -24,9 +36,9 @@ STORAGE(){
     AVAILABLE_STORAGE_SPACE=`df -mT | grep "ext4" | awk '{print $5}'`
     USED_STORAGE_SPACE=`df -mT | grep "ext4" | awk '{print $4}'`
 
-    echo "Total Storage Size: ${TOTAL_STORAGE_SPACE} MB"
-    echo "Total Available Storage: ${AVAILABLE_STORAGE_SPACE} MB"
-    echo -e "Total Used Storage: ${USED_STORAGE_SPACE} MB\n"
+    echo "Total Storage Size: ${TOTAL_STORAGE_SPACE} MB" >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
+    echo "Total Available Storage: ${AVAILABLE_STORAGE_SPACE} MB" >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
+    echo -e "Total Used Storage: ${USED_STORAGE_SPACE} MB\n" >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
 }
 
 #OS: Displays information of OS and it's version
@@ -34,16 +46,17 @@ OS(){
     OS_NAME=`hostnamectl | grep "Operating System" | cut -d" " -f 3`
     OS_VERSION=`hostnamectl | grep "Operating System" | cut -d" " -f 4`
 
-    echo "OS Name: ${OS_NAME}"
-    echo -e "OS Version: ${OS_VERSION}\n"
+    echo "OS Name: ${OS_NAME}" >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
+    echo -e "OS Version: ${OS_VERSION}\n" >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
 }
 
 #IP_ADDRESS: Displays IP address
 IP_ADDRESS(){
     IP=`hostname -I`
 
-    echo -e "IP Address: ${IP}\n"
+    echo -e "IP Address: ${IP}\n" >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
 }
+
 
 #Wrapper Function to call other functions To Display Informations
 DISPLAY(){
@@ -54,3 +67,5 @@ DISPLAY(){
 }
 
 DISPLAY
+
+echo "==========================================" >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
