37a5911e97aa3a465e7d276004b3821153bcc3d4
sakib-celloscope <sakib.celloscope@gmail.com>
2023-09-11 17:11:35 +0600

    Formatted log file in table format

--- System-Info-Logger.sh
+++ System-Info-Logger.sh
@@ -10,7 +10,7 @@ CURRENT_DIRECTORY=`pwd`
 CURRENT_DATE=`date +%F`
 
 #Checking Current Time
-CURRENT_TIME=`date +%T`
+CURRENT_TIME=`date +%T.%3N`
 
 LOG_FILE="Log-${CURRENT_DATE}"
 
@@ -22,27 +22,58 @@ echo -e "Script Executed at ${CURRENT_TIME}\n"
 #Printing the Script Name
 echo -e "${0} Executed at ${CURRENT_TIME}\n" >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
 
+#Printing in Tabular Format
+PRINT_TABLE_ROW(){
+    printf "%-25s : %-25s : %s\n" "$1" "$2" "$3"
+}
+
+KB_TO_GB(){
+    IN_GB=`expr $1 / 1000000`
+    return $IN_GB
+}
+
 #RAM: Displays information of RAM
 RAM(){
-    TOTAL_RAM=`free -mt | grep "Total" | awk '{print $2}'`
-    FREE_RAM_SPACE=`free -mt | grep "Total" | awk '{print $4}'`
-    USED_RAM_SPACE=`free -mt | grep "Total" | awk '{print $3}'`
+    #TOTAL_RAM=`free -t | grep "Total" | awk '{print $1/1024/1024/1024 " GB"}'`
+    RAM_IN_KB=`free | awk '/Mem/ {print $2}'`
+    FREE_RAM_KB=`free | awk '/Mem/ {print $4}'`
+    USED_RAM_KB=`free | awk '/Mem/ {print $3}'`
+    
+    KB_TO_GB "${RAM_IN_KB}"
+    TOTAL_RAM=$?
 
+    KB_TO_GB "${FREE_RAM_KB}"
+    FREE_RAM_SPACE=$?
 
-    echo "Total Ram Size: ${TOTAL_RAM} MB"
-    echo "Total Free Space: ${FREE_RAM_SPACE} MB"
-    echo -e "Total Used Space: ${USED_RAM_SPACE} MB\n"
+    KB_TO_GB "${USED_RAM_KB}"
+    USED_RAM_SPACE=$?
+
+    PRINT_TABLE_ROW "${CURRENT_DATE} ${CURRENT_TIME}" "Total RAM" "${TOTAL_RAM}GB"
+    PRINT_TABLE_ROW "${CURRENT_DATE} ${CURRENT_TIME}" "Total Free Space" "${FREE_RAM_SPACE}GB"
+    PRINT_TABLE_ROW "${CURRENT_DATE} ${CURRENT_TIME}" "Total Used Space" "${USED_RAM_SPACE}GB"
+    PRINT_TABLE_ROW "-----------" "-----------" "-----------"
 }
 
 #STORAGE: Displays information of Storage
 STORAGE(){
-    TOTAL_STORAGE_SPACE=`df -mT | grep "ext4" | awk '{print $3}'`
-    AVAILABLE_STORAGE_SPACE=`df -mT | grep "ext4" | awk '{print $5}'`
-    USED_STORAGE_SPACE=`df -mT | grep "ext4" | awk '{print $4}'`
+    TOTAL_STORAGE_SPACE_IN_KB=`df -T | awk '/ext4/ {print $3}'`
+    AVAILABLE_STORAGE_SPACE_IN_KB=`df -T | awk '/ext4/ {print $5}'`
+    USED_STORAGE_SPACE_IN_KB=`df -T | awk '/ext4/ {print $4}'`
 
-    echo "Total Storage Size: ${TOTAL_STORAGE_SPACE} MB"
-    echo "Total Available Storage: ${AVAILABLE_STORAGE_SPACE} MB"
-    echo -e "Total Used Storage: ${USED_STORAGE_SPACE} MB\n"
+    KB_TO_GB "${TOTAL_STORAGE_SPACE_IN_KB}"
+    TOTAL_STORAGE_SPACE=$?
+
+    KB_TO_GB "${AVAILABLE_STORAGE_SPACE_IN_KB}"
+    AVAILABLE_STORAGE_SPACE=$?
+
+    KB_TO_GB "${USED_STORAGE_SPACE_IN_KB}"
+    USED_STORAGE_SPACE=$?
+
+
+    PRINT_TABLE_ROW "${CURRENT_DATE} ${CURRENT_TIME}" "Total Storage Size" "${TOTAL_STORAGE_SPACE}GB"
+    PRINT_TABLE_ROW "${CURRENT_DATE} ${CURRENT_TIME}" "Total Available Storage" "${AVAILABLE_STORAGE_SPACE}GB"
+    PRINT_TABLE_ROW "${CURRENT_DATE} ${CURRENT_TIME}" "Total Used Storage" "${USED_STORAGE_SPACE}GB"
+    PRINT_TABLE_ROW "-----------" "-----------" "-----------"
 }
 
 #OS: Displays information of OS and it's version
@@ -50,20 +81,25 @@ OS(){
     OS_NAME=`hostnamectl | grep "Operating System" | cut -d" " -f 3`
     OS_VERSION=`hostnamectl | grep "Operating System" | cut -d" " -f 4`
 
-    echo "OS Name: ${OS_NAME}"
-    echo -e "OS Version: ${OS_VERSION}\n"
+    PRINT_TABLE_ROW "${CURRENT_DATE} ${CURRENT_TIME}" "OS Name" "${OS_NAME}"
+    PRINT_TABLE_ROW "${CURRENT_DATE} ${CURRENT_TIME}" "OS Version" "${OS_VERSION}"
+    PRINT_TABLE_ROW "-----------" "-----------" "-----------"
 }
 
 #IP_ADDRESS: Displays IP address
 IP_ADDRESS(){
     IP=`hostname -I`
 
-    echo -e "IP Address: ${IP}\n"
+    #echo -e "IP Address: ${IP}\n"
+
+    PRINT_TABLE_ROW "${CURRENT_DATE} ${CURRENT_TIME}" "IP Address" "${IP}"
 }
 
 
 #Wrapper Function to call other functions To Display Informations
 DISPLAY(){
+    PRINT_TABLE_ROW "TIMESTAMP" "SYSTEM INFO" "VOLUME" >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
+    PRINT_TABLE_ROW "-----------" "-----------" "-----------" >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
     RAM >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
     STORAGE >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
     OS >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
@@ -76,6 +112,6 @@ SEND_MAIL(){
     echo "Here is the system info log file which is generated at ${CURRENT_TIME}. Thank you." | mail -s "System Info Log Update" -A "$LOG_FILE.log" abdullahnazmussakib@gmail.com
 }
 
-SEND_MAIL 
+#SEND_MAIL 
 
-echo "==========================================" >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
+echo "=====================================================================" >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
