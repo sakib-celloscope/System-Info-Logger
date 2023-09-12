ff1f7e9f62c66e6e5f8ad70a68673a9eb528e5da
sakib-celloscope <sakib.celloscope@gmail.com>
2023-09-12 14:37:12 +0600

    fixed timestamp issue

--- System-Info-Logger.sh
+++ System-Info-Logger.sh
@@ -48,9 +48,9 @@ RAM(){
     KB_TO_GB "${USED_RAM_KB}"
     USED_RAM_SPACE=$?
 
-    PRINT_TABLE_ROW "${CURRENT_DATE} ${CURRENT_TIME}" "Total RAM" "${TOTAL_RAM}GB"
-    PRINT_TABLE_ROW "${CURRENT_DATE} ${CURRENT_TIME}" "Total Free Space" "${FREE_RAM_SPACE}GB"
-    PRINT_TABLE_ROW "${CURRENT_DATE} ${CURRENT_TIME}" "Total Used Space" "${USED_RAM_SPACE}GB"
+    PRINT_TABLE_ROW "${CURRENT_DATE} $(date +%T.%3N)" "Total RAM" "${TOTAL_RAM}GB"
+    PRINT_TABLE_ROW "${CURRENT_DATE} $(date +%T.%3N)" "Total Free Space" "${FREE_RAM_SPACE}GB"
+    PRINT_TABLE_ROW "${CURRENT_DATE} $(date +%T.%3N)" "Total Used Space" "${USED_RAM_SPACE}GB"
     PRINT_TABLE_ROW "-----------" "-----------" "-----------"
 }
 
@@ -70,9 +70,9 @@ STORAGE(){
     USED_STORAGE_SPACE=$?
 
 
-    PRINT_TABLE_ROW "${CURRENT_DATE} ${CURRENT_TIME}" "Total Storage Size" "${TOTAL_STORAGE_SPACE}GB"
-    PRINT_TABLE_ROW "${CURRENT_DATE} ${CURRENT_TIME}" "Total Available Storage" "${AVAILABLE_STORAGE_SPACE}GB"
-    PRINT_TABLE_ROW "${CURRENT_DATE} ${CURRENT_TIME}" "Total Used Storage" "${USED_STORAGE_SPACE}GB"
+    PRINT_TABLE_ROW "${CURRENT_DATE} $(date +%T.%3N)" "Total Storage Size" "${TOTAL_STORAGE_SPACE}GB"
+    PRINT_TABLE_ROW "${CURRENT_DATE} $(date +%T.%3N)" "Total Available Storage" "${AVAILABLE_STORAGE_SPACE}GB"
+    PRINT_TABLE_ROW "${CURRENT_DATE} $(date +%T.%3N)" "Total Used Storage" "${USED_STORAGE_SPACE}GB"
     PRINT_TABLE_ROW "-----------" "-----------" "-----------"
 }
 
@@ -81,8 +81,8 @@ OS(){
     OS_NAME=`hostnamectl | grep "Operating System" | cut -d" " -f 3`
     OS_VERSION=`hostnamectl | grep "Operating System" | cut -d" " -f 4`
 
-    PRINT_TABLE_ROW "${CURRENT_DATE} ${CURRENT_TIME}" "OS Name" "${OS_NAME}"
-    PRINT_TABLE_ROW "${CURRENT_DATE} ${CURRENT_TIME}" "OS Version" "${OS_VERSION}"
+    PRINT_TABLE_ROW "${CURRENT_DATE} $(date +%T.%3N)" "OS Name" "${OS_NAME}"
+    PRINT_TABLE_ROW "${CURRENT_DATE} $(date +%T.%3N)" "OS Version" "${OS_VERSION}"
     PRINT_TABLE_ROW "-----------" "-----------" "-----------"
 }
 
@@ -92,7 +92,7 @@ IP_ADDRESS(){
 
     #echo -e "IP Address: ${IP}\n"
 
-    PRINT_TABLE_ROW "${CURRENT_DATE} ${CURRENT_TIME}" "IP Address" "${IP}"
+    PRINT_TABLE_ROW "${CURRENT_DATE} $(date +%T.%3N)" "IP Address" "${IP}"
 }
 
 
@@ -112,6 +112,6 @@ SEND_MAIL(){
     echo "Here is the system info log file which is generated at ${CURRENT_TIME}. Thank you." | mail -s "System Info Log Update" -A "$LOG_FILE.log" abdullahnazmussakib@gmail.com
 }
 
-SEND_MAIL 
+#SEND_MAIL 
 
 echo "=====================================================================" >> ${CURRENT_DIRECTORY}/Log-${CURRENT_DATE}.log
